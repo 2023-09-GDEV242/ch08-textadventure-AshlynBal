6ac46b49de3f9359d2bcd4f5ebab1c179260db86
Ashlyn <AshlynBal04@gmail.com>
2023-10-22 22:55:59 -0400

    Javadoc changes

--- Game.java
+++ Game.java
@@ -11,8 +11,8 @@
  * rooms, creates the parser and starts the game.  It also evaluates and
  * executes the commands that the parser returns.
  *
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author Ashlyn Balicki, Michael Kölling, and David J. Barnes
+ * @version 2023.10.22
  */
 
 public class Game {
--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+/**
+ * @author Ashlyn Balicki
+ * @version 2023.10.22
+ */
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
